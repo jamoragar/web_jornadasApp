5cb630cc619a24f999b029e159928170fb00c57b
Marceloh1987 <55755584+Marceloh1987@users.noreply.github.com>
2020-08-19 20:36:08 -0300

    Paginas de retorno estado pago

---
+++ src/Components/PayReturn/DonarFailure.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Header from "../NavBar/NavBar";
+import Footer from "../Home/Layout/Footer/Footer";
+
+const DonarFailure = (props) => {
+	const initData = {
+		state: "No hemos podido procesar tu pago.",
+        heading1: "Su aporte por",
+        heading2: "CLP no se ha podido llevar a cabo.",
+		content: "Por favor intenta nuevamente, gracias.",
+		btnText: "Volver al inicio",
+	};
+	return (
+		<div className="inner inner-pages">
+			<div className="main">
+				<Header />
+				<section
+					id="home"
+					className="section welcome-area inner-area bg-overlay h-100vh overflow-hidden"
+				>
+					<div className="container h-100">
+						<div className="row align-items-center h-100">
+							<div className="col-12 col-md-8">
+								<div className="welcome-intro">
+									<h1 className="text-danger">{initData.state}</h1>
+									<h3 className="text-white">{`${initData.heading1} $5000 ${initData.heading2}`}</h3>
+									<p className="text-white my-4">{initData.content}</p>
+									<a href="/" className="btn sApp-btn text-uppercase">
+										{initData.btnText}
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+                <Footer />
+			</div>
+		</div>
+	);
+};
+
+export default DonarFailure;
--- src/Components/Routes/Routes.js
+++ src/Components/Routes/Routes.js
@@ -6,6 +6,10 @@ import Contacto from '../Home/Layout/Contacto/Contacto';
 import Header from '../NavBar/NavBar';
 import Dashboard from '../Dashboard/Dashboard';
 import NotFound404 from '../404/404';
+import DonarSuccess from '../PayReturn/DonarSuccess';
+import DonarFailure from '../PayReturn/DonarFailure';
+import BonoSuccess from '../PayReturn/BonoSuccess';
+import BonoFailure from '../PayReturn/BonoFailure';
 
 const Routes = (props) => {
     if(props){
@@ -19,6 +23,10 @@ const Routes = (props) => {
                     <Route path="/Contacto" exact component={Contacto} />
                     <Route path="/Dashboard/:uid" component={() => props.authenticated ? <Dashboard/> : <Home/>} />
                     <Route path='/not-found' component={NotFound404} />
+                    <Route path='/Donar-exito' component={DonarSuccess}/>
+                    <Route path='/Donar-fallo' component={DonarFailure}/>
+                    <Route path='/Bono-exito' component={BonoSuccess}/>
+                    <Route path='/Bono-fallo' component={BonoFailure}/>
                     <Route component={() => <Redirect to='/'/>} />
                 </Switch>
             </BrowserRouter>
